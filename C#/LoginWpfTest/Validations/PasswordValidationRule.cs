3e37129b2e7766424a213ccac1efab35721fcf0d
BbillahrariBBr <mb.billah8@gmail.com>
2023-04-24 13:30:40 +0600

    validation done

---
+++ C#/LoginWpfTest/Validations/PasswordValidationRule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace LoginWpfTest.Validations
+{
+    public class PasswordValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            Regex regex = new Regex("^[A_Za-z0-9!@#$%^&]{5,10}$");
+            if (!regex.IsMatch(value.ToString()))
+            {
+                return new ValidationResult(false, "Please a valid Password");
+            }
+            else
+            {
+                return ValidationResult.ValidResult;
+            }
+        }
+
+     
+    }
+}
---
+++ C#/LoginWpfTest/Validations/UserNameValidationRule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Text.RegularExpressions;
+
+namespace LoginWpfTest.Validations
+{
+   public class UserNameValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            Regex regex = new Regex("^[A_Za-z0-9!@#$%^&]{2,25}$");
+            string input = value.ToString();
+            if (!regex.IsMatch(input))
+            {
+                if(input.Length < 2 && input.Length >25)
+                {
+                    return new ValidationResult(false, "UserName have at least 2 char and max 25 char");
+                }
+                return new ValidationResult(false, "Please a valid User Name");
+            }
+            else
+            {
+                return ValidationResult.ValidResult;
+            }
+        }
+    }
+
+    
+}
