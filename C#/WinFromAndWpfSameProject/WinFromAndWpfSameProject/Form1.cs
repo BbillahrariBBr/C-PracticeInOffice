16110143a85826ffe656e6d9b65d1b40c8735bad
BbillahrariBBr <mb.billah8@gmail.com>
2023-04-26 15:22:10 +0600

    second way done wpf open in winfrom

---
+++ C#/WinFromAndWpfSameProject/WinFromAndWpfSameProject/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+
+namespace WinFromAndWpfSameProject
+{
+    public partial class Form1 : Form
+    {
+        Window window;
+
+
+        public Form1()
+        {
+            this.Visible = false;
+            InitializeComponent();
+            //Hide();
+            UserControl1 usercontrol = new UserControl1();
+           
+            window = new Window
+            {
+                Title = "My User Control Dialog",
+                Content = new UserControl1()
+            };
+
+            window.ShowDialog();
+            Close();
+        }
+
+        public Form1(bool doNotInvisible)
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ C#/WinFromAndWpfSameProject/WinFromAndWpfSameProject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFromAndWpfSameProject
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Form1 form = new Form1();
+            Application.EnableVisualStyles();
+          
+            if(form.Visible == true)
+            {
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+            
+        }
+    }
+}
