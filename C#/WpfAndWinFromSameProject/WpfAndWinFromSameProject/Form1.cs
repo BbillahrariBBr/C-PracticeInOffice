35cfccefd4b5967e123e3978a0c9579a516f4ca8
BbillahrariBBr <mb.billah8@gmail.com>
2023-04-26 11:37:08 +0600

    this.Visibility = Visibility.Hidden;

---
+++ C#/WpfAndWinFromSameProject/WpfAndWinFromSameProject/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WpfAndWinFromSameProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGoto_Click(object sender, EventArgs e)
+        {
+            Hide();
+            MainWindow mainWPf = new MainWindow(true);
+            //mainWPf.Visibility = System.Windows.Visibility.Visible;
+            mainWPf.ShowDialog();
+
+            Close();
+        }
+    }
+}
