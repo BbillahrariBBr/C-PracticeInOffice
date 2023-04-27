17ea1fb0854198b13b33c368aec1ced75bef4f1e
BbillahrariBBr <mb.billah8@gmail.com>
2023-04-27 15:45:43 +0600

    test successfully

---
+++ C#/wpfProjectinWinForm/wpfProjectinWinForm/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//using System.Windows;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace wpfProjectinWinForm
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        //static Mutex mutex = new Mutex(true, "{8F6F0AC4-B9A1-45fd-A8CF-72F04E6BDE8F}");
+        [STAThread]
+        static void Main()
+        {
+            //if (mutex.WaitOne(TimeSpan.Zero, true))
+            //{
+            //    //Application.EnableVisualStyles();
+            //    //Application.SetCompatibleTextRenderingDefault(false);
+            //    //Application.Run(new Form1());
+            //    Application a = new Application();
+            //    a.StartupUri = new Uri("UserControl1", System.UriKind.Relative);
+            //    a.Run();
+            //    mutex.ReleaseMutex();
+            //}
+
+            //else
+            //{
+            //    MessageBox.Show("only one instance at a time");
+            //}
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frm_Home());
+        }
+    }
+}
+   
+   
---
+++ C#/wpfProjectinWinForm/wpfProjectinWinForm/frmMain.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+
+namespace wpfProjectinWinForm
+{
+    public partial class frm_Home : Form
+    {
+        Window window;
+        public frm_Home()
+        {
+            Hide();
+            InitializeComponent();
+            //WpfUserControl.UserControl1 usercontrol = new WpfUserControl.UserControl1();
+            //LoginWpf.LoginUI login = new LoginWpf.LoginUI();
+            window = new Window
+            {
+                Title = "My User Control Dialog",
+                Content = new WpfUserControl.UserControl1()
+            };
+            window.ShowDialog();
+            Close();
+
+        }
+    }
+}
