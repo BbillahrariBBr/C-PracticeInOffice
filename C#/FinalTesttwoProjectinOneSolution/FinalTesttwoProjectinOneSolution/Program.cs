c18003d66effd8bf8aaef9c731e611b5b979b1e8
BbillahrariBBr <mb.billah8@gmail.com>
2023-04-27 18:32:39 +0600

    final design almost complete

---
+++ C#/FinalTesttwoProjectinOneSolution/FinalTesttwoProjectinOneSolution/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalTesttwoProjectinOneSolution
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            frmMain form = new frmMain();
+            Application.EnableVisualStyles();
+
+            if (form.Visible == true)
+            {
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new frmMain());
+            }
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new frmMain());
+            
+            
+        }
+    }
+}
---
+++ C#/FinalTesttwoProjectinOneSolution/FinalTesttwoProjectinOneSolution/frmMain.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+
+namespace FinalTesttwoProjectinOneSolution
+{
+    public partial class frmMain : Form
+    {
+        Window window;
+        public frmMain()
+        {
+            this.Visible = false;
+            Hide();
+            InitializeComponent();
+            //LoginUIWPF.LoginUI usercontrol = new LoginUIWPF.LoginUI();
+
+            window = new Window
+            {
+                //Title = "WOW!!! working",
+                Content = new LoginUIWPF.LoginUI(),
+                Height = 470,
+                Width = 780,
+            };
+
+            window.ShowDialog();
+            Close();
+            Dispose();
+        }
+    }
+}
