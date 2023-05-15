6bcc8bdb7fe822d891b50b1333d71e92df491b22
BbillahrariBBr <mb.billah8@gmail.com>
2023-05-15 17:58:45 +0600

    crystal report without db

--- C#/CRPrac/CRPrac/Form1.Designer.cs
+++ C#/CRPrac/CRPrac/Form1.Designer.cs
@@ -32,8 +32,13 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.tbxFirstName = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tbxLastName = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.tbxUserName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbxPassword = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnShowReport = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -75,13 +80,13 @@
             this.tbxFirstName.Size = new System.Drawing.Size(185, 26);
             this.tbxFirstName.TabIndex = 2;
             // 
-            // textBox1
+            // tbxLastName
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(473, 138);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(185, 26);
-            this.textBox1.TabIndex = 4;
+            this.tbxLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxLastName.Location = new System.Drawing.Point(473, 138);
+            this.tbxLastName.Name = "tbxLastName";
+            this.tbxLastName.Size = new System.Drawing.Size(185, 26);
+            this.tbxLastName.TabIndex = 4;
             // 
             // label2
             // 
@@ -91,14 +96,65 @@
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(90, 20);
             this.label2.TabIndex = 3;
-            this.label2.Text = "First Name:";
+            this.label2.Text = "Last Name:";
+            // 
+            // tbxUserName
+            // 
+            this.tbxUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxUserName.Location = new System.Drawing.Point(118, 185);
+            this.tbxUserName.Name = "tbxUserName";
+            this.tbxUserName.Size = new System.Drawing.Size(549, 26);
+            this.tbxUserName.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "User Name:";
+            // 
+            // tbxPassword
+            // 
+            this.tbxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxPassword.Location = new System.Drawing.Point(118, 238);
+            this.tbxPassword.Name = "tbxPassword";
+            this.tbxPassword.Size = new System.Drawing.Size(549, 26);
+            this.tbxPassword.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 244);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Password:";
+            // 
+            // btnShowReport
+            // 
+            this.btnShowReport.Location = new System.Drawing.Point(359, 300);
+            this.btnShowReport.Name = "btnShowReport";
+            this.btnShowReport.Size = new System.Drawing.Size(126, 39);
+            this.btnShowReport.TabIndex = 9;
+            this.btnShowReport.Text = "Show in Report";
+            this.btnShowReport.UseVisualStyleBackColor = true;
+            this.btnShowReport.Click += new System.EventHandler(this.btnShowReport_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnShowReport);
+            this.Controls.Add(this.tbxPassword);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbxUserName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbxLastName);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.tbxFirstName);
             this.Controls.Add(this.label3);
@@ -118,8 +174,13 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox tbxFirstName;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbxLastName;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxUserName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbxPassword;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnShowReport;
     }
 }
 
--- C#/CRPrac/CRPrac/Form1.cs
+++ C#/CRPrac/CRPrac/Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CRPrac.Reports;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+
 
 namespace CRPrac
 {
@@ -17,9 +21,22 @@ namespace CRPrac
             InitializeComponent();
         }
 
-      
+        private void btnShowReport_Click(object sender, EventArgs e)
+        {
+            ReportShowForm reportShowForm = new ReportShowForm();
+            RecordShowWithOutDB report = new RecordShowWithOutDB();
 
-        
-        
+            TextObject first = (TextObject)report.ReportDefinition.Sections["Section3"].ReportObjects["FirstNameText"];
+            first.Text = tbxFirstName.Text;
+            TextObject last = (TextObject)report.ReportDefinition.Sections["Section3"].ReportObjects["LastNameText"];
+            last.Text = tbxLastName.Text;
+            TextObject user = (TextObject)report.ReportDefinition.Sections["Section3"].ReportObjects["UserNameText"];
+            user.Text = tbxUserName.Text;
+            TextObject pass = (TextObject)report.ReportDefinition.Sections["Section3"].ReportObjects["PasswordText"];
+            pass.Text = tbxPassword.Text;
+
+            reportShowForm.crystalReportViewer1.ReportSource = report;
+            reportShowForm.Show();
+        }
     }
 }
