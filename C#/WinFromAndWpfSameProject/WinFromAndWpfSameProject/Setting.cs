ecdb6a539cac856e4801882cf24a248fdd397d7a
BbillahrariBBr <mb.billah8@gmail.com>
2023-05-22 16:27:07 +0600

    Wpfprc

---
+++ C#/WinFromAndWpfSameProject/WinFromAndWpfSameProject/Setting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace WinFromAndWpfSameProject
+{
+    class Setting
+    {
+        public string iniPath = Application.StartupPath + @"\config.ini";
+
+
+    }
+}
