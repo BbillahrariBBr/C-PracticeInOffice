283e67046417c1e1495c22da2f29f15450165857
BbillahrariBBr <mb.billah8@gmail.com>
2023-06-05 13:09:06 +0600

    wpf controls

--- C#/WpfAll/WpfDemo/WpfDemo/MainWindow.xaml.cs
+++ C#/WpfAll/WpfDemo/WpfDemo/MainWindow.xaml.cs
@@ -20,15 +20,35 @@ namespace WpfDemo
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Person> people = new List<Person>();
         public MainWindow()
         {
             InitializeComponent();
+            people.Add(new Person { FirstName = "Tim", LastName = "Corey" });
+            people.Add(new Person { FirstName = "Joe", LastName = "Smith" });
+            people.Add(new Person { FirstName = "Sue", LastName = "Storm" });
+
+            cmbxMy.ItemsSource = people;
         }
 
         private void BtnSubmit_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show($"Hello {tbxFirstName.Text}");
 
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
         }
     }
 }
